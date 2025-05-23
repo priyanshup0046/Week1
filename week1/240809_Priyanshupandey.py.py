aa8034f7410c708de9c3f53b665da91151376283
priyanshup0046 <priyanshupandey0046@gmail.com>
2025-05-23 14:07:14 +0530

    Update 240809_Priyanshupandey.py.py

--- week1/240809_Priyanshupandey.py.py
+++ week1/240809_Priyanshupandey.py.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 # converting the image in a grey scale due to the fact that the sobel operator is used for edge detection and it is better to use it on a grey scale image (vs code ne automatic type kia lol)
 img = Image.open("mario.jpeg").convert("L")
 img_np = np.array(img)
-# I am using sobe cornel in this case and defining it here as asked 
+# I am using sobe cornel in this case and defining it here as asked to do manually
 sobel_x = np.array([[ -1, 0, 1],[ -2, 0, 2],[ -1, 0, 1]])
 sobel_y = np.array([[ -1, -2, -1],[  0,  0,  0],[  1,  2,  1]])
 
@@ -27,10 +27,7 @@ def convolve(img, kernel):
 grad_x = convolve(img_np, sobel_x)
 grad_y = convolve(img_np, sobel_y)
 magnitude = np.sqrt(grad_x**2 + grad_y**2)
-
-# Load original in color for display
 original_img = Image.open("mario.jpeg")
-# Normalize to 0-255
 magnitude = (magnitude / magnitude.max()) * 255
 magnitude = magnitude.astype(np.uint8)
 
